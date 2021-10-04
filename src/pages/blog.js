47682c6f0b2e5774c3d132e29a532d5390ada9a3
Front End Developer <manasiry3@gmail.com>
2021-10-05 01:35:52 +0530

    changes menu link and page creation two

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,32 +6,33 @@ const Blog = ({ data }) => {
   const allBlog = data.allWpPost
 
   return (
-    <Layout> <div className="mainSpacing">
+    <Layout>
 
       <SEO title="Blog" />
-      <div className="container">
-        <div className="row">
+      <div className="mainSpacing">
+        <div className="container">
+          <div className="row">
 
-          {
-            allBlog.nodes.map((blogP, index) => (
-              <div className="col-md-4 mb-4">
-                <div key={index} className="card">
-                  <div className="card-body">
-                    <h4 className="card-title mb-4">
-                      <Link to={blogP.slug}>
-                        {blogP.title}
-                      </Link>
-                    </h4>
-                    <div className="card-text" dangerouslySetInnerHTML={{ __html: blogP.excerpt }} />
+            {
+              allBlog.nodes.map((blogP, index) => (
+                <div className="col-md-4 mb-4">
+                  <div key={index} className="card">
+                    <div className="card-body">
+                      <h4 className="card-title mb-4">
+                        <Link to={blogP.slug}>
+                          {blogP.title}
+                        </Link>
+                      </h4>
+                      <div className="card-text" dangerouslySetInnerHTML={{ __html: blogP.excerpt }} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
-          }
+              ))
+            }
 
+          </div>
         </div>
       </div>
-    </div>
     </Layout>
   )
 }
