ff2befe67a3d119cb8365b3117e23ffe37d89647
Front End Developer <manasiry3@gmail.com>
2021-10-17 22:50:59 +0530

    404 page add image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 import { Link } from "gatsby"
+import PageNotFound from "../images/pagenotfound.jpg"
 
 const NotFound = () => {
     return (
         <Layout>
+            <figure className="banner-main">
+                <img src={PageNotFound} alt="404 error" />
+            </figure>
             <div className="container mainSpacing">
                 <h1>404: Page Not Found</h1>
                 <p>
